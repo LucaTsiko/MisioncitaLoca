c13437e109cb3aa3a73e8aafecad94c1e8cd0850
Luca Tsiklauri <ltsik@myuax.com>
2026-03-10 10:25:01 +0100

    chore (InterfazConsola): creacion de la clase InterfazConsola con su constructor.

--- src/camareros/InterfazConsola.java
+++ src/camareros/InterfazConsola.java
@@ -1,5 +1,16 @@
 package camareros;
 
+import cocineros.GestionMisiones;
+
 public class InterfazConsola {
+    private GestionMisiones gestor;
+
+    public InterfazConsola(GestionMisiones gestor) {
+        this.gestor = gestor;
+    }
+    // mostrar menú
+    // mostrar respuestas
+    // pedir distintos tipos de datos
+    // llamar a las distintas funcionalidades del gestor
 
 }
