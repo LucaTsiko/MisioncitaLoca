134fd4b0e8eacaac6156394c8e35c890bfaa453a
Luca Tsiklauri <ltsik@myuax.com>
2026-03-10 08:56:26 +0100

    chore (GestionDeMisiones): creacion de la clase GestionDeMisiones con el ArrayList misiones declarado.

---
+++ src/GestionDeMisiones.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class GestionDeMisiones {
+    private ArrayList<Mision> misones;
+
+    public GestionDeMisiones(){
+        this.misones = new ArrayList<>();
+    }
+}
